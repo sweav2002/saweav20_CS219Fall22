f7b4b776cf883a65d48c7cf812ce4b4cd1b1da2b
saweav20 <sweav2002@gmail.com>
2022-08-30 15:14:33 -0400

    update file to include today's changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,22 @@
 // BY convention, class names are capitalized.
 public class HelloWorld {
 
+    /*
+        This is a multiline block comment.
+        another comment line.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+
+        double avagadros = 6.022140857e23;  // scientific notation
+        double electron_mass = 9.1093837015e-31;
+
+
 
     }
 }
