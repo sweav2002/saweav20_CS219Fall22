46b5193f298a3954710cf4bb1f1b6d15ae55d434
saweav20 <sweav2002@gmail.com>
2022-11-30 10:12:58 -0500

    all new notes

---
+++ src/ZipCodeDB/Main.java
@@ -0,0 +1,25 @@
+package ZipCodeDB;
+
+public class Main {
+    public static void main(String[] args) {
+        Zipcode z1 = new Zipcode("13617", "NY", "Canton",
+                75.16, 44.59, 11781);
+        //System.out.println(z1);
+
+
+        Zipcode z2 = new Zipcode("13668", "NY", "Norwood",
+                74.99, 44.93, 4305);
+        //System.out.println(z2);
+
+        Database db = new Database();
+
+        //System.out.println(db.findByZip("13617"));
+        //System.out.println(db.findByZip("hello"));
+
+        System.out.println(db.search("13617"));
+        System.out.println(db.search("13668"));
+
+
+        return;
+    }
+}
---
+++ src/ZipCodeDB/Zipcode.java
@@ -0,0 +1,74 @@
+package ZipCodeDB;
+
+/*
+ *  Hold data about one zipcode
+ */
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Zipcode implements Comparable<Zipcode>, Distance {
+
+    private String code;
+    private String state; // two character code abbreviation
+    private String city;
+    private double lng;
+    private double lat;
+    private int pop;
+
+    public Zipcode(String code, String state, String city, double lng, double lat, int pop) {
+        this.code = code;
+        this.state = state;
+        this.city = city;
+        this.lng = lng;
+        this.lat = lat;
+        this.pop = pop;
+    }
+
+    public String toString() {
+        return "Zipcode{" +
+                "code='" + code + '\'' +
+                ", state='" + state + '\'' +
+                ", city='" + city + '\'' +
+                ", lng=" + lng +
+                ", lat=" + lat +
+                ", pop=" + pop +
+                '}';
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    // the natural ordering for zipcode
+    @Override
+    public int compareTo(Zipcode zc) {
+        return this.code.compareTo(zc.code);
+    }
+
+    @Override
+    public double distance(Zipcode zip) {
+        return 0;  // fancy great circle distance
+    }
+
+    // http://api.geonames.org/findNearByWeatherJSON?formatted=true&lat=44&lng=-74&username=edharcourt
+    public WeatherObservation getWeatherData() {
+        URL url = null;    // null is the "nothing value"
+        Scanner s = null;
+
+        String path = "http://api.geonames.org/findNearByWeatherJSON?formatted=true&lat=" + this.lat + "&lng=" + this.lng +
+                "&username=edharcourt\n";
+
+        try {
+            url = new URL("http://api.geonames.org/findNearByWeatherJSON?formatted=true&lat=44&lng=-74&username=edharcourt"); // create a URL object for the path
+            s = new Scanner(url.openConnection().getInputStream());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
